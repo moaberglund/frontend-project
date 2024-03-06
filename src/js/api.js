67cc6f05e3681ca1a47a778d2f52a3ef73b66e0a
Moa Berglund <moa.21@hotmail.com>
2024-03-06 12:49:38 +0100

    js för sökning av år kopplad till första api:et

--- src/js/api.js
+++ src/js/api.js
@@ -1,8 +1,71 @@
 "use strict";
 
+
+//sök
+const searchYear = document.getElementById("search-input");
+const submitYear = document.getElementById("submit");
+const message = document.getElementById("message");
+searchYear.addEventListener("input", checkInput);
+submitYear.addEventListener("click", yearThroughAPI);
+
+window.onload = init;
+
+function init() {
+    //inaktivera sökknappen
+    submitYear.disabled = true;
+    
+    // Lyssna på formulärets submit-händelse
+    document.querySelector('form').addEventListener('submit', function(e) {
+        e.preventDefault(); // Förhindra formuläret från att skickas innan kontrollen är klar
+        yearThroughAPI(); //anropa funktion vid submit
+    });
+}
+
+//funktion för input
+function checkInput() {
+    const input = searchYear.value;
+    //kontrollera längd
+    if (input.length < 4) {
+        message.innerHTML = "Ange ett år i formatet YYYY.";
+        submitYear.disabled = true;
+    } else {
+        message.innerHTML = "";
+        submitYear.disabled = false;
+    }
+}
+
+
+// Funktion för att hämta och behandla data från API:et
+async function yearThroughAPI() {
+    try {
+        const result = await fetchAPI();
+        console.log("Hämtad data av Nobelpristagare: ", result);
+
+        //kolla om det finns 0, en eller flera mottagare av pris 
+        if (result.nobelPrizes.length > 0) {
+            const laureates = result.nobelPrizes[0].laureates;
+            if (laureates.length > 0) {
+                //spara författaren/-na i en array
+                const authors = [];
+                laureates.forEach(author => {
+                    authors.push(author.fullName.en)
+                });
+                console.log("Författare: ", authors);
+
+            } else {
+                console.log("Inga mottagare av priset detta år...")
+            }
+        }
+    } catch (error) {
+        console.error('Process error:', error);
+    }
+}
+
+
+
 //asynkron hämtning av api Nobelpriset
 async function fetchAPI() {
-    const year = 1974;
+    const year = searchYear.value;
     const url = `https://api.nobelprize.org/2.1/nobelPrizes?nobelPrizeCategory=lit&nobelPrizeYear=${year}&format=json&csvLang=se`;
 
     try {
